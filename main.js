b73474786ec9154a51c1d4f8211cb0fb558d09fd
s-walrus <swalrus@yandex.ru>
2018-05-04 17:13:08 +0300

    Change direction when direction symbols are entered

--- main.js
+++ main.js
@@ -2,25 +2,30 @@
 var direction = 0
 var pointer_x = 0
 var pointer_y = 0
+var len = 9
 
-var arr = new Array(9);
-for (i = 0; i < 9; i++) {
-  arr[i] = new Array(9);
+var grid = new Array(len);
+var map = new Array(len);
+for (i = 0; i < len; i++) {
+  grid[i] = new Array(len);
+  map[i] = new Array(len);
 }
 
 $(document).ready(function() {
-  for (x = 1; x < 10; x++)
-    for (y = 1; y < 10; y++) {
-      $("#bef-box").append('<input class="cell" id="' + (x-1) + '-' + (y-1) + '" type="text" style="' +
+  for (x = 1; x < len+1; x++)
+    for (y = 1; y < len+1; y++) {
+      $("#bef-box").append('<input class="cell"' +
+                            'id="' + (x-1) + '-' + (y-1) +
+                            '" type="text" style="' +
                             'grid-column-start: ' + x + ';' +
                             'grid-column-end: ' + x + ';' +
                             'grid-row-start: ' + y + ';' +
                             'grid-row-end: ' + y + ';' +
                             '"/>');
-      arr[x-1][y-1] = $('#' + (x-1) + '-' + (y-1));
+      grid[x-1][y-1] = $('#' + (x-1) + '-' + (y-1));
 
       // Move cursor to the end of input
-      arr[x-1][y-1].on("click", function() {
+      grid[x-1][y-1].on("click", function() {
         val = $(this).val()
         $(this).val("");
         $(this).val(val);
@@ -30,15 +35,45 @@ $(document).ready(function() {
         pointer_y = temp[1];
       });
 
-      arr[x-1][y-1].on("input", function() {
+      grid[x-1][y-1].on("input", function() {
         // Cut chars except for the last one
         $(this).val($(this).val().slice(-1));
-        // TODO if char is direction, change direction
-        // Move pointer
-        switch(direction) {
+
+        val = $(this).val();
+        map[pointer_x][pointer_y] = val;
+
+        switch (val) {
+          case '^':
+            direction = 0;
+            break;
+          case '>':
+            direction = 1;
+            break;
+          case 'v':
+            direction = 2;
+            break;
+          case '<':
+            direction = 3;
+            break;
+        }
+
+        switch (direction) {
           case 0:
-            arr[pointer_x][pointer_y-1].focus();
+            grid[pointer_x][pointer_y-1].focus();
             pointer_y--;
+            break;
+          case 1:
+            grid[pointer_x+1][pointer_y].focus();
+            pointer_x++;
+            break;
+          case 2:
+            grid[pointer_x][pointer_y+1].focus();
+            pointer_y++;
+            break;
+          case 3:
+            grid[pointer_x-1][pointer_y].focus();
+            pointer_x--;
+            break;
         }
       });
     }
