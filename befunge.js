c35192a05e5cadcee49fa0d614312d9f7648596d
s-walrus <swalrus@yandex.ru>
2018-05-10 17:22:08 +0300

    Added some Befunge commands (all except for input and output)

--- befunge.js
+++ befunge.js
@@ -3,6 +3,7 @@
 */
 
 var stack = [];
+var string_mode = false;
 
 function updateStack() {
 	$("#stack-box").html("");
@@ -12,79 +13,120 @@ function updateStack() {
 }
 
 function bef_do(com) {
-  switch (com) {
-			
-    case '@':
-      stop();
-			return 0;
-			
-		case '0':
-		case '1':
-		case '2':
-		case '3':
-		case '4':
-		case '5':
-		case '6':
-		case '7':
-		case '8':
-		case '9':
-			stack.push(parseInt(com));
-			break;
-			
-		case '+':
-			stack.push(stack.pop() + stack.pop());
-			break;
-		case '-':
-			stack.push(-(stack.pop() - stack.pop()));
-			break;
-		case '*':
-			stack.push(stack.pop() * stack.pop());
-			break;
-		case '/':
-			stack.push(1 / stack.pop() * stack.pop());
-			break;
-		case '%':
-			a = stack.pop();
-			stack.push(stack.pop() % a);
-			break;
-			
-		case '!':
-			if (stack.pop() == 0) {
-				stack.push(1);
-			} else {
-				stack.push(0);
-			}
-			break;
+	if (string_mode && com != '"') {
+		stack.push(com.charCodeAt(0));
+	} else {
+		switch (com) {
+				
+			case '@':
+				stop();
+				return 0;
+				
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				stack.push(parseInt(com));
+				break;
+				
+			case '+':
+				stack.push(stack.pop() + stack.pop());
+				break;
+			case '-':
+				stack.push(-(stack.pop() - stack.pop()));
+				break;
+			case '*':
+				stack.push(stack.pop() * stack.pop());
+				break;
+			case '/':
+				stack.push(1 / stack.pop() * stack.pop());
+				break;
+			case '%':
+				a = stack.pop();
+				stack.push(stack.pop() % a);
+				break;
+				
+			case '!':
+				if (stack.pop() == 0) {
+					stack.push(1);
+				} else {
+					stack.push(0);
+				}
+				break;
 
-		case '`':
-			// if a > b in [..., a, b]
-			if (stack.pop() < stack.pop()) {
-				stack.push(1);
-			} else {
-				stack.push(0);
-			}
-			break;
+			case '`':
+				// if a > b in [..., a, b]
+				if (stack.pop() < stack.pop()) {
+					stack.push(1);
+				} else {
+					stack.push(0);
+				}
+				break;
 
-		case '?':
-			changeDirection(Math.floor(Math.random() * 4));
-			break;
+			case '?':
+				changeDirection(Math.floor(Math.random() * 4));
+				break;
 
-		case '_':
-			if (stack.pop() == 0) {
-				changeDirection('>');
-			} else {
-				changeDirection('<');
-			}
-			break;
+			case '_':
+				if (stack.pop() == 0) {
+					changeDirection('>');
+				} else {
+					changeDirection('<');
+				}
+				break;
+				
+			case '|':
+				if (stack.pop() == 0) {
+					changeDirection('v');
+				} else {
+					changeDirection('^');
+				}
+				break;
+				
+			case '"':
+				string_mode = !string_mode;
+				break;
 			
-		case '|':
-			if (stack.pop() == 0) {
-				changeDirection('v');
-			} else {
-				changeDirection('^');
-			}
-			break;
-  }
+			case ':':
+				a = stack.pop();
+				stack.push(a, a);
+				break;
+				
+			case '\\':
+				a = stack.pop();
+				b = stack.pop();
+				stack.push(a, b);
+				break;
+				
+			case '$':
+				stack.pop();
+				break;
+				
+			case '#':
+				move();
+				break;
+				
+			case 'p':
+				val = stack.pop();
+				y = stack.pop();
+				x = stack.pop();
+				map[x][y] = val.toString().charCodeAt(0);
+				grid[x][y].val(String.fromCharCode(val));
+				break;
+				
+			case 'g':
+				y = stack.pop();
+				x = stack.pop();
+				stack.push(map[x][y].charCodeAt(0));
+				break;
+		}
+	}
 	updateStack();
 	changeDirection(com);
 	move();
